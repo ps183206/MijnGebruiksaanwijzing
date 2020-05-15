c0369e656ae12ddef23f4ac98737c370631718f8
jouw naam <jordygerrits690@gmail.com>
2020-05-15 11:30:37 +0200

    Database Class aangemaakt, Xaml Spelerbeheer werkend gekregen en backend van nieuw persoon verder aan gewerkt

---
+++ MijnGebruiksaanwijzing/Classes/DatabaseConnection.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+
+namespace MijnGebruiksaanwijzing.Classes
+{
+    class DatabaseConnection
+    {
+        MySqlConnection conn =
+            new MySqlConnection("Server=localhost;Database=mijngebruiksaanwijzing;Uid=root;Pwd=");
+        int role = 1;
+
+        public void AddPerson(string naam, string achternaam, string username, string password, bool admin)
+        {
+            if(admin == true)
+            {
+                role = 0;
+            }            
+            conn.Open();
+            MySqlCommand command = conn.CreateCommand();
+
+            command.CommandText = "INSERT INTO `users` (`UserID`, `FirstName`, `LastName`, `UserName`, `Password`, `Role`) VALUES(NULL, '" + naam + "', '" + achternaam + "', '" + username + "', '" + password + "', '" + role + "');";
+            command.ExecuteReader();
+
+            conn.Close();
+
+        }
+    }
+}
