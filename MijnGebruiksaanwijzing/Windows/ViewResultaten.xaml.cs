db676109b2aba14ac2588918292d1de497a5bfbf
patrick huizinga <ps181584@summacollege.nl>
2020-06-05 10:11:33 +0200

    redcards xaml verbeterd, zo simpel mogelijk

---
+++ MijnGebruiksaanwijzing/Windows/ViewResultaten.xaml.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MijnGebruiksaanwijzing.Windows
+{
+    /// <summary>
+    /// Interaction logic for ViewResultaten.xaml
+    /// </summary>
+    /// 
+    public partial class ViewResultaten : Window
+    {
+        private MySqlConnection conn;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public ViewResultaten()
+        {
+            server = "localhost";
+            database = "mijngebruiksaanwijzing";
+            uid = "root";
+            password = "";
+
+            string connString;
+            connString = $"SERVER={server};DATABASE={database};UID={uid};PASSWORD={password};";
+
+            conn = new MySqlConnection(connString);
+
+            InitializeComponent();
+        }
+    }
+}
