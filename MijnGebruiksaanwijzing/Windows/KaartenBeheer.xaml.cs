29f998708d88c60c423dba6a986369d94140e963
jouw naam <jordygerrits690@gmail.com>
2020-06-13 16:51:11 +0200

    Bezig geweest met Kaartenbeheer morgen maak ik het verder af

--- MijnGebruiksaanwijzing/Windows/KaartenBeheer.xaml.cs
+++ MijnGebruiksaanwijzing/Windows/KaartenBeheer.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+using System.Data;
 
 namespace MijnGebruiksaanwijzing.Windows
 {
@@ -19,16 +22,36 @@ namespace MijnGebruiksaanwijzing.Windows
     /// </summary>
     public partial class KaartenBeheer : Window
     {
+
+        
         public KaartenBeheer()
         {
             InitializeComponent();
+            List<string> ListData = new List<string>();
             //CardID = AI
             //CardDesc = Description Content van de kaarten
             //CardColor = Cardkleur
             //CardType = Stage/School
 
+            MySqlConnection conn =
+            new MySqlConnection("Server=localhost;Database=mijngebruiksaanwijzing;Uid=root;Pwd=");
 
+            conn.Open();
+            MySqlCommand command = conn.CreateCommand();
 
+            command.CommandText = "SELECT CardDesc FROM `cards` ";
+            MySqlDataReader reader = command.ExecuteReader();
+            DataTable dtData = new DataTable();
+            dtData.Load(reader);
+            conn.Close();
+            foreach (DataRow row in dtData.Rows)
+            {
+                foreach (DataColumn col in dtData.Columns)
+                {
+                    ListData.Add(row[col].ToString());
+                }
+            }
+            ListKaarten.ItemsSource = ListData;
             //Zorgen dat alle listviews gevuld worden met de gegevens vanuit de database
             //terugknop werkend maken
 
@@ -38,5 +61,13 @@ namespace MijnGebruiksaanwijzing.Windows
 
             // mogelijkheid om een kaart te verwijderen
         }
+        public class AddData
+        {
+            //Database connectie maken
+            
+            //Database gegevens ophalen met query
+            //Gegevens in de listview zetten
+        }
+
     }
 }
